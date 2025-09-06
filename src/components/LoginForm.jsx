d11f279be15dea58fefdfc4f11892ec654e14a19
Festus Muia <fstsmuia@gmail.com>
2025-09-06 16:19:44 +0300

    add: base solution

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,30 @@
+const LoginForm = (props) => (
+    <div>
+        <h2>log in to application</h2>
+        <form onSubmit={props.handleLogin}>
+            <div>
+                <label>
+                    username
+                    <input
+                        type="text" 
+                        value={props.username}
+                        onChange={props.handleUsernameChange} 
+                    />
+                </label>
+            </div>
+            <div>
+                <label>
+                    password
+                    <input
+                        type="password" 
+                        value={props.password}
+                        onChange={props.handlePasswordChange} 
+                    />
+                </label>
+            </div>
+            <button type="submit">login</button>
+        </form>
+    </div>
+)
+
+export default LoginForm
