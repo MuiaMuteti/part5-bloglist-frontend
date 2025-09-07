5f16a15c8359bc8ed311f7f032785117e132cf25
Festus Muia <fstsmuia@gmail.com>
2025-09-07 14:08:02 +0300

    add: frontend login functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +16,42 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = async event => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch(error) {
+      console.log('invalid credentials', error.message)
+    }
+  }
+
+  const handleUsernameChange = event => {
+    setUsername(event.target.value)
+  }
+  const handlePasswordChange = event => {
+    setPassword(event.target.value)
+  }
+
+  if (user === null) {
+    return (
+      <LoginForm
+        username={username}
+        password={password}
+        handleLogin={handleLogin}
+        handleUsernameChange={handleUsernameChange}
+        handlePasswordChange={handlePasswordChange}
+      />
+    )
+  }
+
   return (
     <div>
       <h2>blogs</h2>
+      <p>{user.name} logged in</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
