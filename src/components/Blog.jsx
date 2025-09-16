cbb5b3b415edee8d0003e09c10da7ca71efe01bd
Festus Muia <fstsmuia@gmail.com>
2025-09-16 16:31:02 +0300

    add: button to view or hide blog details

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,33 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleDetailsVisibility = () => {
+    setDetailsVisible(!detailsVisible)
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={toggleDetailsVisibility}>{ detailsVisible? "hide": "view"}</button>
+      {detailsVisible && (
+        <div>
+          <p>{blog.url}</p>
+          <p>likes {blog.likes} <button>like</button></p>
+          <p>{blog.user?.name}</p>
+        </div>
+      )}
+    </div>  
+  )
+} 
 
 export default Blog
